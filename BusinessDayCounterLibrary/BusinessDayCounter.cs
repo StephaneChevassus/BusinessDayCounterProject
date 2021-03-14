4cb60cdc61e386351f99d38d32ce75ddc8bde24b
Stephane Chevassus <stephane@giveeasy.org>
2021-03-14 15:24:55 +1100

    Added stub like comments before starting the implementation

--- BusinessDayCounterLibrary/BusinessDayCounter.cs
+++ BusinessDayCounterLibrary/BusinessDayCounter.cs
@@ -5,14 +5,34 @@ namespace BusinessDayCounterLibrary
 {
     public static class BusinessDayCounter
     {
+        /// <summary>
+        /// Caculates the number of weekdays between two dates
+        /// </summary>
         public static int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            //todo
+            //Compare supplied dates
+
+            //Get the date range between the two dates excluding firstDate and secondDate
+
+            //Count the number of dates that are a weekday "Monday, Tuesday, Wednesday, Thursday, Friday"
+
+            //Return the count
             return -1;
         }
+
+        /// <summary>
+        /// Caculates the number of business days between two dates given a list of public holidays
+        /// </summary>
         public static int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
         {
-            //todo
+            //Compare supplied dates
+
+            //Get the date range between the two dates excluding firstDate and secondDate
+
+            //Count the number of dates that are a business day
+            //"Monday, Tuesday, Wednesday, Thursday, Friday" but excluding public holidays
+
+            //Return the count
             return -1;
         }
     }
