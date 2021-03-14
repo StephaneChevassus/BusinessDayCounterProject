5b76934bb6a28b667cdaca3ca1a862c016817b13
Stephane Chevassus <stephane@giveeasy.org>
2021-03-14 15:02:12 +1100

    Added Tests for Task Two as per requirements

--- BusinessDayCounterTests/TestBusinessDayCounter.cs
+++ BusinessDayCounterTests/TestBusinessDayCounter.cs
@@ -1,6 +1,7 @@
 using BusinessDayCounterLibrary;
 using System;
 using System.Globalization;
+using System.Linq;
 using Xunit;
 
 namespace BusinessDayCounterTests
@@ -25,6 +26,32 @@ namespace BusinessDayCounterTests
             var actualCount = BusinessDayCounter.WeekdaysBetweenTwoDates(testFirstDate, testSecondtDate);
 
             //Then the count should exclude weekends
+            Assert.Equal(expectedCount, actualCount);
+        }
+
+        /// <summary>
+        /// Ensure that the business days between two dates are calculated correctly given a list of public holidays for Task 2
+        /// </summary>
+        [Theory]
+        [InlineData("7/10/2013", "9/10/2013", 1)]
+        [InlineData("24/12/2013", "27/12/2013", 0)]
+        [InlineData("7/10/2013", "1/01/2014", 59)]
+        public void Task2_Calculate_BusinessDays_Between_TwoDates(string firstDate, string secondDate, int expectedCount)
+        {
+            //Given two dates and a list of public holidays
+            var publicHolidays = new[]
+            {
+                    new DateTime(2013, 12, 25),
+                    new DateTime(2013, 12, 26),
+                    new DateTime(2014, 1, 1)
+            };
+            var testFirstDate = DateTime.ParseExact(firstDate, "d/M/yyyy", CultureInfo.InvariantCulture);
+            var testSecondtDate = DateTime.ParseExact(secondDate, "d/M/yyyy", CultureInfo.InvariantCulture);
+
+            //When I count the business days between the two dates
+            var actualCount = BusinessDayCounter.BusinessDaysBetweenTwoDates(testFirstDate, testSecondtDate, publicHolidays.ToList());
+
+            //Then the count should exclude weekends and public holidays
             Assert.Equal(expectedCount, actualCount);
         }
     }
